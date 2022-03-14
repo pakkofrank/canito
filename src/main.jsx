c77c92dc9faf67200537ce1d6f1f174f17971ffc
pakkofrank <jpalma@ucol.mx>
2022-03-13 22:22:09 -0600

    canito

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+import Menu from "./components/Menu";
+
+import Router from "./components/Router";
+
+ReactDOM.render(
+  <div>
+    <Menu />
+    <Router />
+  </div>,
+  document.getElementById("root")
+);
---
+++ src/pages/MamitasPage.jsx
@@ -0,0 +1,3 @@
+export default function MamitasPage() {
+  return <div>MamitasPage</div>;
+}
